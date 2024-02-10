e0bbc63a14012149b3d455b3211738d5f030ec07
Deepak Arora <arroadeepak0817@gmail.com>
2024-02-10 18:45:46 +0530

    second part completed-UI design

---
+++ frontend/src/pages/signup/GenderCheckbox.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const GenderCheckbox = () => {
+  return (
+    <div className='flex'>
+    <div className='form-control'>
+        <label className='label gap-2 cursor-pointer'>
+            <span className='label-text'>Male</span>
+            <input type='checkbox' className='checkbox border-gray-300'></input>
+        </label>
+    </div>
+
+    <div className='form-control'>
+        <label className='label gap-2 cursor-pointer'>
+            <span className='label-text'>Female</span>
+            <input type='checkbox' className='checkbox border-gray-300'></input>
+
+        </label>
+    </div>
+      
+    </div>
+  )
+}
+
+export default GenderCheckbox
